e142bc1e8582c6c2df3d0914796982d2b2a5d801
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-05-30 13:51:45 +0530

    Update flask_rds_user_api.py

--- 4.Python-backend-testing/flask_rds_user_api.py
+++ 4.Python-backend-testing/flask_rds_user_api.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 # Database Configuration
 db_config = {
-    'host': 'database-1.cytea2me860o.us-east-1.rds.amazonaws.com',
+    'host': 'database-1.cytea2me860o.us-east-1.rds.amazonaws.com', #update red endpoint
     'user': 'admin',
-    'password': 'Cloud123',
+    'password': 'yaswanth',
     'database': 'dev'  # Change to your actual database name
 }
 
