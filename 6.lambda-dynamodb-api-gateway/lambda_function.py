e2a44dfad3be0755c0c46084fc8799fde0d16858
Arumulla Yaswanth Reddy <164642237+arumullayaswanth@users.noreply.github.com>
2025-05-31 13:54:41 +0530

    Update lambda_function.py

--- 6.lambda-dynamodb-api-gateway/lambda_function.py
+++ 6.lambda-dynamodb-api-gateway/lambda_function.py
@@ -47,7 +47,7 @@ def page_router(httpmethod, querystring, formbody):
 def insert_record(formbody):
     formbody = formbody.replace("=", "' : '")
     formbody = formbody.replace("&", "', '")
-    formbody = "INSERT INTO veera value {'" + formbody + "'}"
+    formbody = "INSERT INTO yaswanth value {'" + formbody + "'}"   # Replace with DynamoDB Table name(My table name is Yashwant)
 
     client = boto3.client('dynamodb')
     response = client.execute_statement(Statement=formbody)
